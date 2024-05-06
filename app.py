cc0d3962770a00f4d80f6d15046838c98cdd2e27
FilippoPietroNeri <neri.filippo@iisgalvanimi.edu.it>
2024-05-06 09:00:59 +0000

    AAAAa

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import pandas as pd
-
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    print(request.args.get('error'))
+    if request.args.get('error'):
+        print("ok")
+        return render_template('index.html', error=request.args.get('error'))
     return render_template('index.html')
+
+@app.route('/test')
+def homepaget():
+    return render_template('login.html')
 
 @app.route('/login', methods=['POST'])
 def loginhandle():
     if request.method == "POST":
-        data = request.json
-        print(data)
+        databaseutenti = pd.read_csv('databaseutenti.csv')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        checkUser = databaseutenti[(databaseutenti['Username'] == username) & (databaseutenti['Password'] == password)]
+        if len(checkUser) == 0:
+            return redirect('/?error=Invalid username or password.', code=302)
+        return render_template('result.html', username=username)
 
+@app.route('/login/fetch', methods=['POST'])
+def login2handle():
+    if request.method == "POST":
+        databaseutenti = pd.read_csv('databaseutenti.csv')
+        json = request.get_json()
+        username = json.get('username')
+        password = json.get('password')
+        
+        checkUser = databaseutenti[(databaseutenti['Username'] == username) & (databaseutenti['Password'] == password)]
+        if len(checkUser) == 0:
+            return redirect('/test?error=Invalid username or password.', code=302)
+        return render_template('result.html', username=username)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3246, debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,18 @@
 
-function loginInfo()
+function login()
 {
     const username = document.getElementById('username');
     const password = document.getElementById('password');
 
-    fetch('https://3246-filippopietr-flasklogin-zeuacy53xgb.ws-eu110.gitpod.io/login', {
+    fetch('https://3246-filippopietr-flasklogin-yqyywfzh7xl.ws-eu111.gitpod.io/login/fetch', {
         method: 'POST',
-        body: {
-            username,
-            password
-        }
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username: username.value,
+            password: password.value,
+        })
     }).then((response) => response.json())
     .then((data) => {
         console.log(data);
