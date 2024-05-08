3e5ebbbf1887342198565d59787d698573f29579
FilippoPietroNeri <neri.filippo@iisgalvanimi.edu.it>
2024-05-08 07:12:10 +0000

    Progetto finito

--- app.py
+++ app.py
@@ -4,27 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    print(request.args.get('error'))
-    if request.args.get('error'):
-        print("ok")
-        return render_template('index.html', error=request.args.get('error'))
-    return render_template('index.html')
-
-@app.route('/test')
-def homepaget():
     return render_template('login.html')
-
-@app.route('/login', methods=['POST'])
-def loginhandle():
-    if request.method == "POST":
-        databaseutenti = pd.read_csv('databaseutenti.csv')
-        username = request.form.get('username')
-        password = request.form.get('password')
-        
-        checkUser = databaseutenti[(databaseutenti['Username'] == username) & (databaseutenti['Password'] == password)]
-        if len(checkUser) == 0:
-            return redirect('/?error=Invalid username or password.', code=302)
-        return render_template('result.html', username=username)
 
 @app.route('/login/fetch', methods=['POST'])
 def login2handle():
@@ -36,8 +16,8 @@ def login2handle():
         
         checkUser = databaseutenti[(databaseutenti['Username'] == username) & (databaseutenti['Password'] == password)]
         if len(checkUser) == 0:
-            return redirect('/test?error=Invalid username or password.', code=302)
-        return render_template('result.html', username=username)
+            return { "code": 302, "message": "Invalid Username or Password" }
+        return { "code": 200, "message": "Logged", "user": checkUser.to_json() }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3246, debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,11 @@
+var alert = document.querySelector('#alert');
 
 function login()
 {
     const username = document.getElementById('username');
     const password = document.getElementById('password');
 
-    fetch('https://3246-filippopietr-flasklogin-yqyywfzh7xl.ws-eu111.gitpod.io/login/fetch', {
+    fetch('https://3246-filippopietr-flasklogin-q9kbk79e0gr.ws-eu111.gitpod.io/login/fetch', {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
@@ -15,6 +16,17 @@ function login()
         })
     }).then((response) => response.json())
     .then((data) => {
-        console.log(data);
+        if (data.code == 200) {
+            const user = JSON.parse(data.user);
+            alert.style.display = "block";
+            alert.classList.remove("alert-danger");
+            alert.classList.add("alert-success");
+            alert.innerHTML = `Logged as <b>${user.Nome[0]} ${user.Cognome[0]}</b>`;
+        } else if (data.code == 302) {
+            alert.style.display = "block";
+            alert.classList.remove("alert-success");
+            alert.classList.add("alert-danger");
+            alert.innerHTML = data.message;
+        }
     })
 }
